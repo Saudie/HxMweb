0481f036929b994ca266d64d7300a7cd12202dc8
Saudie <74901133+Saudie@users.noreply.github.com>
2021-05-11 10:37:12 +0200

    Update heartFunctions.js

--- heartFunctions.js
+++ heartFunctions.js
@@ -181,3 +181,8 @@ function getRandomInt(min, max) {
 
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function popup {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
